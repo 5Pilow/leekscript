b05b765706c1f8012ea5e8bc0396333587f9ca20
Pilow <pierre.laupretre@gmail.com>
2019-07-21 21:05:13 +0200

    [test][file] Add project euler 22

--- test/files.cpp
+++ test/files.cpp
@@ -70,6 +70,7 @@ void Test::test_files() {
 	file("test/code/euler/pe019.leek").equals("171");
 	file("test/code/euler/pe020.leek").equals("648");
 	file("test/code/euler/pe021.leek").equals("31626");
+	file("test/code/euler/pe022.leek").equals("871198282");
 	DISABLED_file("test/code/euler/pe062.leek").equals("127035954683");
 	file("test/code/euler/pe063.leek").equals("49");
 	DISABLED_file("test/code/euler/pe206.leek").equals("1389019170");
