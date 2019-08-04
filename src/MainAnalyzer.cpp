0ea15e7caad208d7d6427cd18f1d6b40cd1fb7a3
Pilow <pierre.laupretre@gmail.com>
2019-08-04 23:34:20 +0200

    [analyzer] Create a analyze() method for analyzer-web

--- src/MainAnalyzer.cpp
+++ src/MainAnalyzer.cpp
@@ -1,6 +1,24 @@
 #include "CLI.hpp"
+#include "environment/Environment.hpp"
+
+#if EMCC
+#include <emscripten/emscripten.h>
+
+extern "C" {
+const char* EMSCRIPTEN_KEEPALIVE analyze(char* code) {
+	ls::Environment env;
+	ls::Program program { env, code, "snippet" };
+	env.analyze(program);
+	std::ostringstream oss;
+	program.print(oss, true);
+	return oss.str().c_str();
+}
+}
+#else
 
 int main(int argc, char* argv[]) {
 	ls::CLI cli;
 	return cli.start_analyzer(argc, argv);
 }
+
+#endif
