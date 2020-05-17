57487baf91034ed38a6b720f41b5972aa7ae882b
Pilow <pierre.laupretre@gmail.com>
2020-05-17 23:56:37 +0200

    [token] Fix token position

--- src/analyzer/lexical/Token.cpp
+++ src/analyzer/lexical/Token.cpp
@@ -3,7 +3,8 @@
 
 namespace ls {
 
-Token::Token(TokenType type, File* file, size_t raw, size_t line, size_t character, const std::string& content) : type(type), content(content), location(file, {line, character - content.size() - 1, raw - content.size() - 1}, {line, character, raw - 1}) {
+Token::Token(TokenType type, File* file, size_t raw, size_t line, size_t character, const std::string& content)
+ : type(type), content(content), location(file, {line, character - content.size() - 1, raw - content.size() - 1}, {line, character - 2, raw - 2}) {
 
 	if (type == TokenType::STRING) {
 		this->location.start.column--;
